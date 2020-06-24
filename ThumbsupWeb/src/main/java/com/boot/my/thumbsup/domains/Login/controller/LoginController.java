fd15e3d232198ccd8d57a388a3b2050b9948fd3b
user name <user email>
2020-06-24 17:16:19 +0900

    Web index Degisn, Login Fix

--- ThumbsupWeb/src/main/java/com/boot/my/thumbsup/domains/Login/controller/LoginController.java
+++ ThumbsupWeb/src/main/java/com/boot/my/thumbsup/domains/Login/controller/LoginController.java
@@ -37,11 +37,10 @@ public class LoginController {
     public String login(Model model) {
     	return "index";
     }
-
     @PostMapping(value="/userLogin")
     @ResponseBody
     public ModelAndView LoginAPI(
-    		ModelAndView mv,
+    			ModelAndView mv,
     			String url,
     			String id,
     			String pwd,
@@ -87,28 +86,97 @@ public class LoginController {
 			// 요청에 대한 데이터 실패값 : no_data
 			if(responseDtl.equals("no_data")) {
 				redirectAttributes.addFlashAttribute( "msg", "아이디 또는 비밀번호를 확인해주세요." );
+				redirectAttributes.addFlashAttribute( "loginRslt", "fail" );
 				return new ModelAndView("redirect:/index");
-				
+			} else if(responseDtl.equals("error")) {
+				redirectAttributes.addFlashAttribute( "loginRslt", "error" );
+				return new ModelAndView("redirect:/index");
 			} else {
 				redirectAttributes.addFlashAttribute( "token", responseDtl );
-				redirectAttributes.addFlashAttribute( "msg", "어서오세요 !" );
+				redirectAttributes.addFlashAttribute( "loginRslt", "success" );
 				return new ModelAndView("redirect:/index");
-				
 			}
-			
 		} catch (Exception e) {
 			System.out.println("API 데이터 요청 및 응답 실패");
 			System.out.println("e :  "+e); 
 			
 		}
-		
     	return mv;
-    	
     }
     
-    
-    
-    
+    /*
+     * 회원가입
+     */
+    @RequestMapping(value="/userRegister")
+    @ResponseBody
+    public ModelAndView RegisterAPI(
+    			ModelAndView mv,
+    			String url,
+    			String id_reg,
+    			String pwd_reg,
+    			String name,
+    			String rrno,
+    			String gender,
+    			RestTemplate restTemplate,
+    			RedirectAttributes redirectAttributes,
+    			@RequestBody String msg
+    		) {
+    	String responseDtl = null;
+
+    	System.out.println("member --- msg@@@@@@@@@@@@@@@@@ : "+msg);
+    	
+    	System.out.println("member --- Register, API로 요청");
+    	// API 데이터 요청 및 응답
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			//Charset utf8 = Charset.forName("UTF-8");
+			//MediaType mediaType = new MediaType("application","json",utf8);
+			//headers.setContentType(mediaType);
+			System.out.println("member --- #headers# "+headers);
+	
+			//요청 url
+			url = "http://localhost:8007/service/register";
+			
+			MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+			map.add("id", id_reg);
+			map.add("pwd", pwd_reg);
+			map.add("name", name);
+			map.add("rrno", rrno);
+			map.add("gender", gender);
+
+			HttpEntity<MultiValueMap<String,String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+			System.out.println("member --- #entity# "+entity);
+			//ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+			ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+			//response
+			//응답데이터 가져오기
+			responseDtl = response.getBody();
+			System.out.println("----- 응답 DATA ----- "+responseDtl);
+			
+			// .getStatusCode = HTTP 상태코드 반환, 200:오류없이 전송성공
+			// .hasBody = body유무 반환 true or false
+			System.out.println("Result - status ("+ response.getStatusCode() + ") has body: " + response.hasBody());
+			
+			// 요청에 대한 데이터 실패값 : no_data
+			if(responseDtl.equals("no_data")) {
+				redirectAttributes.addFlashAttribute( "msg", "아이디 또는 비밀번호를 확인해주세요." );
+				redirectAttributes.addFlashAttribute( "loginRslt", "fail" );
+				return new ModelAndView("redirect:/index");
+			} else if(responseDtl.equals("error")) {
+				redirectAttributes.addFlashAttribute( "loginRslt", "error" );
+				return new ModelAndView("redirect:/index");
+			} else {
+				redirectAttributes.addFlashAttribute( "token", responseDtl );
+				redirectAttributes.addFlashAttribute( "loginRslt", "success" );
+				return new ModelAndView("redirect:/index");
+			}
+		} catch (Exception e) {
+			System.out.println("API 데이터 요청 및 응답 실패");
+			System.out.println("e :  "+e); 
+			
+		}
+    	return mv;
+    }
     
     
     /*
