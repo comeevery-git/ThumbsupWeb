4e59df30fb0f02a722293ae05d21ea81c64af22f
user name <user email>
2020-07-02 17:49:20 +0900

    Login, Register Button modal

--- ThumbsupWeb/src/main/resources/static/assets/js/index.js
+++ ThumbsupWeb/src/main/resources/static/assets/js/index.js
@@ -1,3 +1,61 @@
+/* Modal */
+// Get the modal
+	(function($) {
+        var modal = document.getElementById('myLoginModal');
+ 
+        // Get the button that opens the modal
+        var btn = document.getElementById("login_btn");
+ 
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];                                          
+ 
+        // When the user clicks on the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+ 
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+ 
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+	})(jQuery);
+	(function($) {
+        var modal = document.getElementById('myRegisterModal');
+ 
+        // Get the button that opens the modal
+        var btn = document.getElementById("register_btn");
+ 
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];                                          
+ 
+        // When the user clicks on the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+ 
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+ 
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+	})(jQuery);
+
+	
+
+
 /*
 	Editorial by HTML5 UP
 	html5up.net | @ajlkn
